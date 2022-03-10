d007e3b0f5779a2c0cf6f9eaebeeaaf1315fa2e7
brandynbennett <brandynlbennett@gmail.com>
2022-03-10 16:47:10 -0700

    module attribute timeout

--- lib/api_client_fun/services/user.ex
+++ lib/api_client_fun/services/user.ex
@@ -2,10 +2,11 @@ defmodule ApiClientFun.Services.User do
   @behaviour ApiClientFun.Services.UserBehaviour
 
   @user_url "https://blooming-savannah-20593.herokuapp.com/api/users"
+  @timeout 1000
 
   @impl true
   def list_users do
-    {:ok, response} = HTTPoison.get(@user_url, [], recv_timeout: 1000)
+    {:ok, response} = HTTPoison.get(@user_url, [], recv_timeout: @timeout)
     {:ok, Jason.decode!(response.body)["users"]}
   end
 
