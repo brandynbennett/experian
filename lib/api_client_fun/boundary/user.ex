c235118f029331d92d0531a6fd96e496e8cbe087
brandynbennett <brandynlbennett@gmail.com>
2022-03-10 16:29:22 -0700

    conver to genserver

--- lib/api_client_fun/boundary/user.ex
+++ lib/api_client_fun/boundary/user.ex
@@ -1,16 +1,39 @@
-defmodule ApiClientFun.Boundary.User do
+defmodule ApiClientFun.Boundary.UserRepo do
+  use GenServer
+
   alias ApiClientFun.Core.{User, Profile}
 
-  def profile_for_name(name) when is_binary(name) do
-    {:ok, user_data} = user_service().list_users()
-
-    create_users(user_data)
-    |> Enum.find(&(&1.name == name))
-    |> Map.get(:profile)
+  def new(users \\ [], name \\ __MODULE__) do
+    GenServer.start_link(__MODULE__, users, name: name)
   end
 
-  def profile_for_name(name) do
-    {:error, "profile_for_name/1 expects a string, got #{name}"}
+  @doc """
+  Get a user's profile
+  """
+  def profile_for_name(server \\ __MODULE__, name) do
+    GenServer.call(server, {:profile_for_name, name})
+  end
+
+  @impl true
+  def init(users \\ []) do
+    {:ok, users}
+  end
+
+  @impl true
+  def handle_call({:profile_for_name, name}, _from, _users) when is_binary(name) do
+    {:ok, user_data} = user_service().list_users()
+
+    users = create_users(user_data)
+
+    profile =
+      Enum.find(users, &(&1.name == name))
+      |> Map.get(:profile)
+
+    {:reply, profile, users}
+  end
+
+  def handle_call({:profile_for_name, name}, _from, users) do
+    {:reply, {:error, "profile_for_name/1 expects a string, got #{name}"}, users}
   end
 
   defp user_service do
--- test/boundary/user_test.exs
+++ test/boundary/user_test.exs
@@ -1,22 +1,23 @@
-defmodule ApiClientFun.Boundary.UserTest do
+defmodule ApiClientFun.Boundary.UserRepoTest do
   use ExUnit.Case, async: true
-  alias ApiClientFun.Boundary.User
+  alias ApiClientFun.Boundary.UserRepo
   alias ApiClientFun.Core.Profile
 
   import Mox
   setup :verify_on_exit!
 
   test "profile_for_name/1 error if not string" do
-    # ApiClientFun.Services.UserMock
-    # |> expect(:list_users, fn ->
-    #   {:ok, [%{}]}
-    # end)
+    UserRepo.new()
 
-    assert User.profile_for_name(:foo) ==
+    assert UserRepo.profile_for_name(:foo) ==
              {:error, "profile_for_name/1 expects a string, got #{:foo}"}
   end
 
   test "profile_for_name/1 gets all users if they are not cached" do
+    {:ok, pid} = UserRepo.new()
+
+    allow(ApiClientFun.Services.UserMock, self(), pid)
+
     ApiClientFun.Services.UserMock
     |> expect(:list_users, fn ->
       {:ok,
@@ -28,6 +29,6 @@ defmodule ApiClientFun.Boundary.UserTest do
     end)
 
     assert %Profile{age: 25, gender: "M", planet: "Earth", species: "Human", status: "Alive"} =
-             User.profile_for_name("Philip J Fry")
+             UserRepo.profile_for_name("Philip J Fry")
   end
 end
