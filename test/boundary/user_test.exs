69424e6f30c026ba32bfce85ed9db6cb6011924a
brandynbennett <brandynlbennett@gmail.com>
2022-03-10 13:59:39 -0700

    get error test passing

--- test/boundary/user_test.exs
+++ test/boundary/user_test.exs
@@ -6,10 +6,10 @@ defmodule ApiClientFun.Boundary.UserTest do
   setup :verify_on_exit!
 
   test "profile_for_name/1 error if not string" do
-    ApiClientFun.Services.UserMock
-    |> expect(:profile_for_name, fn :foo ->
-      {:error, "profile_for_name/1 expects a string, got #{:foo}"}
-    end)
+    # ApiClientFun.Services.UserMock
+    # |> expect(:list_users, fn ->
+    #   {:ok, [%{}]}
+    # end)
 
     assert User.profile_for_name(:foo) ==
              {:error, "profile_for_name/1 expects a string, got #{:foo}"}
