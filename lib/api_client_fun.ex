510273b6dd7bd8c1f086bc42ec36385bd5f94ed8
brandynbennett <brandynlbennett@gmail.com>
2022-03-10 16:59:36 -0700

    make public api

--- lib/api_client_fun.ex
+++ lib/api_client_fun.ex
@@ -1,18 +1,15 @@
 defmodule ApiClientFun do
   @moduledoc """
-  Documentation for `ApiClientFun`.
+  Find user data
   """
+
+  alias ApiClientFun.Boundary.UserRepo
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ApiClientFun.hello()
-      :world
-
+  Get a user's profile from their name
   """
-  def hello do
-    :world
+  def profile_for_name(name) do
+    UserRepo.new()
+    UserRepo.profile_for_name(name)
   end
 end
