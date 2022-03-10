c1da2afc40b4e8d43261611582eec475253ad869
brandynbennett <brandynlbennett@gmail.com>
2022-03-10 15:30:49 -0700

    convert return data to structs

--- lib/api_client_fun/boundary/user.ex
+++ lib/api_client_fun/boundary/user.ex
@@ -1,6 +1,9 @@
 defmodule ApiClientFun.Boundary.User do
+  alias ApiClientFun.Core.{User, Profile}
+
   def profile_for_name(name) when is_binary(name) do
-    user_service().list_users()
+    {:ok, user_data} = user_service().list_users()
+    create_users(user_data["users"])
   end
 
   def profile_for_name(name) do
@@ -10,4 +13,25 @@ defmodule ApiClientFun.Boundary.User do
   defp user_service do
     Application.get_env(:api_client_fun, :user_service)
   end
+
+  defp create_users(data) do
+    Enum.map(data, fn raw ->
+      user_data_to_field_list(raw)
+      |> User.new()
+    end)
+  end
+
+  defp user_data_to_field_list(raw_data) do
+    Map.keys(raw_data)
+    |> Enum.reduce(Keyword.new(), fn key, acc ->
+      Keyword.put(acc, String.to_existing_atom(key), extract_value(key, raw_data[key]))
+    end)
+  end
+
+  defp extract_value("profile", value) do
+    user_data_to_field_list(value)
+    |> Profile.new()
+  end
+
+  defp extract_value(_key, value), do: value
 end
--- lib/api_client_fun/services/user.ex
+++ lib/api_client_fun/services/user.ex
@@ -1,12 +1,12 @@
 defmodule ApiClientFun.Services.User do
   @behaviour ApiClientFun.Services.UserBehaviour
 
-  # @user_url "https://blooming-savannah-20593.herokuapp.com/api/users"
+  @user_url "https://blooming-savannah-20593.herokuapp.com/api/users"
 
   @impl true
   def list_users do
-    # HTTPoison.get!(@user_url, [], recv_timeout: 1000)
-    {:ok, [%{}]}
+    {:ok, response} = HTTPoison.get(@user_url, [], recv_timeout: 1000)
+    {:ok, Jason.decode!(response.body)}
   end
 
   @impl true
