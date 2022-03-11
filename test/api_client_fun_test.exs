895c80e79fea1f3f2d3d5515348e95ca10994ee1
brandynbennett <brandynlbennett@gmail.com>
2022-03-10 17:03:18 -0700

    test public api

--- test/api_client_fun_test.exs
+++ test/api_client_fun_test.exs
@@ -1,8 +1,26 @@
 defmodule ApiClientFunTest do
   use ExUnit.Case
-  doctest ApiClientFun
 
-  test "greets the world" do
-    assert ApiClientFun.hello() == :world
+  alias ApiClientFun.Core.Profile
+
+  import Mox
+  setup :verify_on_exit!
+
+  test "profile_for_name/1 returns the profile" do
+    {:ok, pid} = ApiClientFun.Boundary.UserRepo.new()
+
+    allow(ApiClientFun.Services.UserMock, self(), pid)
+
+    ApiClientFun.Services.UserMock
+    |> expect(:list_users, fn ->
+      {:ok, [Jason.decode!(response_fixture())]}
+    end)
+
+    assert %Profile{age: 25, gender: "M", planet: "Earth", species: "Human", status: "Alive"} =
+             ApiClientFun.profile_for_name("Philip J Fry")
+  end
+
+  defp response_fixture do
+    ~s({"company":"Planet Express","id":1,"name":"Philip J Fry","position":"Delivery Boy","profile":{"age":25,"gender":"M","planet":"Earth","species":"Human","status":"Alive"}})
   end
 end
